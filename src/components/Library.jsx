2a6ddf353078c6e0618372852b4bb145f2dad233
wadebooth <wademgmt@gmail.com>
2022-07-06 16:36:39 -0400

    added lofi categories

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -14,7 +14,17 @@ const Library = ({
     return track.category === 'hip-hop'
   })
 
-  return <div>Library</div>
+  return (
+    <div className={`library-container ${librarystatus ? 'show-library' : ''}`}>
+      <div className='library-nav'>
+        <h2>Library</h2>
+        <select onChange={(e) => setSelectedCategory(e.target.value)}>
+          <option value='default'>Lofi Category:</option>
+          <option value='hip-hop'>hip-hop</option>
+        </select>
+      </div>
+    </div>
+  )
 }
 
 export default Library
